c02194b568228fa5f5a9096550e5c144306e598a
ornitorrincco <ornitorrincco@gmail.com>
2018-10-07 15:46:10 -0700

    file structure added

--- DnDApp/index.js
+++ DnDApp/index.js
@@ -1,11 +1,9 @@
-/** @format */
-
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
 XMLHttpRequest = GLOBAL.originalXMLHttpRequest ?
 GLOBAL.originalXMLHttpRequest : GLOBAL.XMLHttpRequest;
-console.disableYellowBox = true;
+// console.disableYellowBox = true;
 
 AppRegistry.registerComponent(appName, () => App);
