2f740e265a8fbbb7d6cba7155f79486b848f589b
ornitorrincco <ornitorrincco@gmail.com>
2018-10-06 20:17:00 -0700

    init project

---
+++ DnDApp/index.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+
+XMLHttpRequest = GLOBAL.originalXMLHttpRequest ?
+GLOBAL.originalXMLHttpRequest : GLOBAL.XMLHttpRequest;
+console.disableYellowBox = true;
+
+AppRegistry.registerComponent(appName, () => App);
